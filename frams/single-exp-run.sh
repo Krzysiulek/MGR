5f4d5c1bb11371c9d128ee2603548b9567d8e063
Krzysztof Czarnecki <czarneckikrzysztof97@gmail.com>
2022-06-16 23:11:42 +0200

    Runner dla slurma

--- frams/single-exp-run.sh
+++ frams/single-exp-run.sh
@@ -10,3 +10,7 @@ echo "Running application"
 cd /home/inf136224/MGR/frams
 python3 FramsticksEvolutionSlurmThreadsRunner.py -path /home/inf136224/MGR/Framsticks50rc20 -sim "eval-allcriteria.sim;deterministic.sim;sample-period-longest.sim" -opt velocity -max_numparts 30 -max_numgenochars 50 -initialgenotype "X(X[N],X[N])" -popsize 100 -generations 50 -hof_size 1
 echo "Application executed"
+
+# rm -rf /Users/krzysztofczarnecki/Documents/studia/MGR/frams/data/pcx_0.2_pmut_0.9_popsize_100 && rm -rf /Users/krzysztofczarnecki/Documents/studia/MGR/frams/data/pcx_0.2_pmut_0.9_popsize_50 && scp -r inf136224@polluks.cs.put.poznan.pl:/home/inf136224/MGR/frams/data/pcx_0.2_pmut_0.9_popsize_100 /Users/krzysztofczarnecki/Documents/studia/MGR/frams/data/pcx_0.2_pmut_0.9_popsize_100 && scp -r inf136224@polluks.cs.put.poznan.pl:/home/inf136224/MGR/frams/data/pcx_0.2_pmut_0.9_popsize_50 /Users/krzysztofczarnecki/Documents/studia/MGR/frams/data/pcx_0.2_pmut_0.9_popsize_50
+# Riafah3eesho
+# watch -n 1 squeue -u inf136224
