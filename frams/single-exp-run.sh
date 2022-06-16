abaa638e734360e464e389d4448c7aaaa4756562
Krzysztof Czarnecki <czarneckikrzysztof97@gmail.com>
2022-06-16 21:13:39 +0200

    Runner dla slurma

---
+++ frams/single-exp-run.sh
@@ -0,0 +1,4 @@
+export PYTHONUNBUFFERED=1
+export DIR_WITH_FRAMS_LIBRARY=/home/inf136224/MGR/Framsticks50rc20
+
+python3 FramsticksEvolutionRunner.py -path /home/inf136224/MGR/Framsticks50rc20 -sim "eval-allcriteria.sim;deterministic.sim;sample-period-2.sim" -opt velocity -max_numparts 30 -max_numgenochars 50 -initialgenotype "X(X[N],X[N])" -popsize 100 -generations 50 -hof_size 1
