31c7a438684ac8f24b36fda1b7e3cc96a7bdf17b
Krzysztof Czarnecki <czarneckikrzysztof97@gmail.com>
2022-06-16 22:09:36 +0200

    Runner dla slurma

--- frams/single-exp-run.sh
+++ frams/single-exp-run.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
-
 echo "Starting to execute"
 
 export PYTHONUNBUFFERED=1
 export DIR_WITH_FRAMS_LIBRARY=/home/inf136224/MGR/Framsticks50rc20
 
-echo "Installing required libraries"
-#pip3 install deap
-#pip3 install numpy
-echo "Installing required libraries ended"
-
 echo "Running application"
-python3 FramsticksEvolutionSlurmRunner.py -path /home/inf136224/MGR/Framsticks50rc20 -sim "eval-allcriteria.sim;deterministic.sim;sample-period-longest.sim" -opt velocity -max_numparts 30 -max_numgenochars 50 -initialgenotype "X(X[N],X[N])" -popsize 100 -generations 50 -hof_size 1
+cd /home/inf136224/MGR/frams
+python3 FramsticksEvolutionSlurmThreadsRunner.py -path /home/inf136224/MGR/Framsticks50rc20 -sim "eval-allcriteria.sim;deterministic.sim;sample-period-longest.sim" -opt velocity -max_numparts 30 -max_numgenochars 50 -initialgenotype "X(X[N],X[N])" -popsize 100 -generations 50 -hof_size 1
 echo "Application executed"
