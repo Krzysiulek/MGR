480e4b46458570094d64d4ac2e39990147e5f435
Krzysztof Czarnecki <czarneckikrzysztof97@gmail.com>
2022-06-29 00:39:22 +0200

    dane do systemu kolejkowego

---
+++ frams/jobs_copier.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for i in {1..5}
+do
+   cp jobs/locked_0_diploid_do_kopiowania.json jobs/ready_${i}_diploid_0.json
+   cp jobs/locked_0_haploid_do_kopiowania.json jobs/ready_${i}_haploid_0.json
+done
